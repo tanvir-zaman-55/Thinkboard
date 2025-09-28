62f4f701af321b20f17d820f92d4dfb8af79fb58
Tanvir Zaman <tanvirzaman@Tanvirs-MacBook-Air.local>
2025-09-28 22:20:36 +0600

    prepared for the deployment

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,30 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
 
-import notesRoutes from './routes/notesRoutes.js'
-import { connectDB } from './config/db.js'
-import rateLimiter from './middleware/rateLimiter.js';
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
-const app = express()
-const PORT = process.env.PORT || 5001
+const app = express();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-// middleware
-app.use(cors({
-    origin:"http://localhost:5173"
-    }// you can just use origin() unless you want to specify which frontend
-));
-app.use(express.json());//this method will parse JSON bodies: req.body
+if (process.env.NODE_ENV !== "production") {
+  // middleware
+  app.use(
+    cors(
+      {
+        origin: "http://localhost:5173",
+      } // you can just use origin() unless you want to specify which frontend
+    )
+  );
+}
+app.use(express.json()); //this method will parse JSON bodies: req.body
 app.use(rateLimiter);
-
 
 //simple custom middleware
 // app.use((req, res, next) => {
@@ -26,13 +32,20 @@ app.use(rateLimiter);
 //     next();
 // })
 
-app.use("/api/notes", notesRoutes )
+app.use("/api/notes", notesRoutes);
 
-//connectDB(); this also works 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist"))); //go to backend directory and then root and then frontend and then dist
 
-connectDB().then(() => { //this is better
-    app.listen(PORT, () => {
-    console.log("Hello from server 5001"
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html")); //in production server the react application in this file at index.html
+  });
+}
+//connectDB(); this also works
 
-    )});
-})
+connectDB().then(() => {
+  //this is better
+  app.listen(PORT, () => {
+    console.log("Hello from server 5001");
+  });
+});
